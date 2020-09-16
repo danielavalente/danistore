089e8462997981617c2f402b59b557945f46d877
Dani Valente <ddani.daraujo@gmail.com>
2020-09-16 21:27:25 +0100

    Project created

---
+++ src/main/java/com/danivalente/CursodaniApplication.java
@@ -0,0 +1,12 @@
+package com.danivalente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursodaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursodaniApplication.class, args);
+	}
+}
---
+++ src/test/java/com/danivalente/CursodaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danivalente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursodaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

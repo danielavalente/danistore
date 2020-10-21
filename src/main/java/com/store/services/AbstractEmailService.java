243f8ccc6c6bd429308d92048b01c9f1f895a71b
Dani Valente <ddani.daraujo@gmail.com>
2020-10-21 19:16:49 +0100

    Forgot password

--- src/main/java/com/store/services/AbstractEmailService.java
+++ src/main/java/com/store/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.store.domain.Client;
 import com.store.domain.Order;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -30,7 +31,6 @@ public abstract class AbstractEmailService implements EmailService {
 	public void sendOrderConfirmationEmail(Order obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromOrder(obj);
 		sendEmail(sm);
-		
 	}
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromOrder(Order obj) {
@@ -68,6 +68,22 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplateOrder(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Client client, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(client, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Client client, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(client.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Here's your new Password");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("New Password: " + newPass);
+		return sm;	
 	}
 	
 }
--- src/main/java/com/store/services/EmailService.java
+++ src/main/java/com/store/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.store.domain.Client;
 import com.store.domain.Order;
 
 public interface EmailService {
@@ -15,5 +16,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Order obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Client client, String newPass);
 
 }

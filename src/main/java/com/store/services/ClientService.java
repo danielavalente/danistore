fe6f5c9f76fdd85b72d4ca1e2a22d7d08522155e
Dani Valente <ddani.daraujo@gmail.com>
2020-10-22 20:32:56 +0100

    Creating image name pattern

--- src/main/java/com/store/services/ClientService.java
+++ src/main/java/com/store/services/ClientService.java
@@ -1,10 +1,12 @@
 package com.store.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClientService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	// Find All
 	public List<Client> findAll() {
@@ -122,7 +130,7 @@ public class ClientService {
 		newObj.setEmail(obj.getEmail());
 	}
 	
-	//Upload client photo
+	//Upload client photo and create personalized name
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
 		
 		UserSS user = UserService.authenticated();
@@ -130,13 +138,10 @@ public class ClientService {
 			throw new AuthorizationException("Access denied");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		Client cli = repo.findByEmail(user.getUsername());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 
 }

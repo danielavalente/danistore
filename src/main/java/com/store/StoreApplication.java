01b20ebb17ffa5d5bcf8a0e4ee08b1d3fed79aa6
Dani Valente <ddani.daraujo@gmail.com>
2020-10-23 16:15:29 +0100

    Exposing header authorization

--- src/main/java/com/store/StoreApplication.java
+++ src/main/java/com/store/StoreApplication.java
@@ -1,17 +1,11 @@
 package com.store;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.store.services.S3Service;
-
 @SpringBootApplication
 public class StoreApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
